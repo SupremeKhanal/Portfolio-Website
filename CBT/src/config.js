5ca0ee84161662505a8f461cc935b3a57fe15327
Supreme Khanal <khanalsupreme3@gmail.com>
2026-08-21 01:58:49 +0545

    added new

--- CBT/src/config.js
+++ CBT/src/config.js
@@ -3,12 +3,13 @@
  * Gemini keys are NOT stored in this file.
  */
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCyZ6wk1ZtFQSc69gpC1gf6taBHKp1rpJM",
+  authDomain: "://firebaseapp.com",
+  projectId: "cbt-portal-3bf52",
+  storageBucket: "cbt-portal-3bf52.firebasestorage.app",
+  messagingSenderId: "929686502565",
+  appId: "1:929686502565:web:eb8c3e2fe16699c31b614c",
+  measurementId: "G-VJ1M46KCSQ"
 };
 
 export const adminUids = [];
